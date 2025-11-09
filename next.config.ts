b1ed5475676523c192cc994a01628f6a2a8951ea
Emilio Beyer-Shelton <EmilioB-S@Hotmail.com>
2025-11-09 16:17:20 +0100

    change basePath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/Form-Test",
+  basePath: "/Form-Test/docs",
   output: "export",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
